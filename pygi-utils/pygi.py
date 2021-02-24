81b660da008d6c9ef7f4041f47bae5d759249c1e
DralrinResthal <c.neilclack@gmail.com>
2021-02-24 13:08:00 -0600

    Clone, add, commit, push

--- pygi-utils/pygi.py
+++ pygi-utils/pygi.py
@@ -1,77 +1,72 @@
+from dotenv import load_dotenv
 import git
-from sys import exc_info
-from logger import CustomLogger
+import sys
+import os
 
-e_log = CustomLogger('e_log', 'error.log', level=logging.DEBUG).create_logger()
+load_dotenv()
+
+token = os.getenv("ACCESS_TOKEN")
 
 
-def clone(uri: str, name: str) -> None:
-    """Clone a git repo
+def clone(uri: str, target: str, token: str) -> None:
+    """Clone a private repo with access token
 
-    uri: String: Github repository uri
-    name: String: Repository name to clone to
+    uri: Repository uri less "https://"
+    target: local directory name target; what will you call local?
+    token: Github access token string
+
+    example uri "github.com/DralrinResthal/ScanSlated-Portal"
+
     """
+    remote = f"https://{token}:x-oauth-basic@{owner}/{repo}.git"
     try:
-        git.Repo.clone_from(uri, name)
-    except:
-        e_log.warning(exc_info())
-
-
-def commit(repo: str, files: list, message: str) -> None:
-    """Add and commit changes to the repository
-
-    repo: String name of the repository, local name
-    files: List: List of files to commit
-    message: String: Commit message string
-    """
-    repo = git.Repo(repo)
-    try:
-        repo.index.add(files)
-        return "Staged changes"
-    except CacheError as e:
-        e_log.warning(e)
-    except:
-        e_log.warning(exc_info())
-
-    try:
-        repo.index.commit(message)
-        return "Commit Successful"
-    except CacheError as e:
-        e_log.warning(e)
-    except:
-        e_log.warning(exc_info())
-
-
-def read_file(filename: str, change: str) -> None:
-    """Rewrite entire file with new contents
-
-    change: String: String formatted file contents
-    At this time, please include the entire new contents
-    of the file, including non-changed text
-    """
-    try:
-        with open(filename, "w") as f:
-            f.write(change)
-            f.close()
-    except:
-        e_log.warning(exc_info())
-
-
-def new_branch(repo: str, name: str) -> None:
-    '''Create a new branch and immediately checkout
-
-    repo: String name of repository (local name)
-    name: String name of new branch
-    '''
-    try:
-        repo = git.Repo(repo)
-    except:
-        e_log.warning(exc_info())
-    try:
-        repo.git.branch(name)
-        repo.git.checkout(name)
-        print(f"New branch {name} created and checked out")
+        git.Repo.clone_from(remote, target)
     except GitError as e:
-        e_log.warning(e)
+        return e
     except:
-        e_log.warning(exc_info())
+        return sys.exc_info()
+
+
+def new_branch(repo: str, branch: str) -> None:
+    '''Creates and switches to a new branch
+
+    repo: Local repo name
+    branch: Name of new branch
+    '''
+    repo = git.Repo(local_repo)
+    try:
+        new_branch = repo.create_head(branch_name)
+    except:
+        return()
+    new_branch.checkout()
+
+
+def update_file(filename: str, content: str) -> None:
+    '''Overwrite the entire file contents with new content
+    Please provide the ENTIRE file contents each time this
+    function is called
+
+    filename: Name of file to chagne, preffered full path if possible
+    content: New contents of the file in string format
+    '''
+    with open(filename, 'w') as f:
+        f.write(content)
+        f.close()
+
+def add_commit(repo: str, changes: list, message: str) -> None:
+    """Stage all changes and commit them in one single step.
+    
+    repo: local name of repository
+    changes: List of all filenames to stage
+    message: Commit message string
+    """
+    # Initialize repository
+    repo = git.Repo(repo)
+    # Stage chagnes
+    repo.index.add(changes)
+    # Commit staged changes
+    repo.index.commit(message)
+    # Push to remote
+    repo.git.push('--set-upstream', repo.remote('origin'), repo.head.ref)
+
+    
