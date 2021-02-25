122747e92c4918e6c619a3b451a224b1a0051437
DralrinResthal <c.neilclack@gmail.com>
2021-02-25 14:09:40 -0600

    Init file.py

---
+++ file/file.py
@@ -0,0 +1,24 @@
+import json
+
+
+def read_yaml():
+    # Read the yaml file
+    pass
+
+def convert_yaml():
+    # Convert yaml to JSON
+    # Return JSON to client
+    pass
+
+def convert_json():
+    # Receive JSON
+    # Convert JSON to yaml
+    pass
+
+def save_newfile():
+    # Save new yaml to file
+    pass
+
+def check_secret():
+    # Check if new config is set to secret
+    pass
