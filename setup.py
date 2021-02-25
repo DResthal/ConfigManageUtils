df8f9380b952df8813002a52c63079a372d27662
DralrinResthal <c.neilclack@gmail.com>
2021-02-24 18:15:22 -0600

    Add dev to setup.py and remove requirements.txt

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
         'PyGithub>=1.54.1',
         'python-dotenv>=0.15.0'
     ],
+    extras_require={
+        'dev': [
+            'black>=20.8b1',
+        ]
+    },
     setup_requires=['pytest-runner>=5.3.0'],
     test_requires=['pytest>=6.2.2'],
     test_suite='tests',
