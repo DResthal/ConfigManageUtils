61a08a2de08b4fa461291876cf900b3e4371abbc
DralrinResthal <c.neilclack@gmail.com>
2021-02-25 19:53:08 -0600

    Add read_file() function

--- file/file.py
+++ file/file.py
@@ -1,23 +1,47 @@
 import json
+import ruamel.yaml
+import sys
 
 
-def read_yaml():
-    # Read the yaml file
-    pass
+def read_yaml(filename: str) -> None:
+    """Reads yaml file which is converted to
+    json by ruamel.yaml natively.
 
-def convert_yaml():
-    # Convert yaml to JSON
-    # Return JSON to client
-    pass
+    filename: String filename to convert
+    """
+    yaml = ruamel.yaml.YAML(typ="safe")
+    try:
+        with open(filename, "r") as f:
+            data = f.read()
+            try:
+                content = yaml.load(data)
+            except raumel.yaml.YAMLError as e:
+                e_msg = f"Error loading yaml file. \n\n{e}"
+                return e_msg
+    except FileNotFoundError as e:
+        return f"File Not Found: {e}"
+    except:
+        return f"{sys.exc_info()}"
+
+    try:
+        content = json.dumps(content)
+        return content
+    except TypeError as e:
+        return e
+    except:
+        message = f"Unknown Error parsing {filename} to json. \n\n {sys.exc_info()}"
+        return message
+
 
 def convert_json():
-    # Receive JSON
     # Convert JSON to yaml
     pass
+
 
 def save_newfile():
     # Save new yaml to file
     pass
+
 
 def check_secret():
     # Check if new config is set to secret
