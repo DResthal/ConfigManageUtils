60b3aef7be6af6d183cc9a0b2e8ffde70f1610a0
DralrinResthal <c.neilclack@gmail.com>
2021-03-01 12:41:16 -0600

    Encrypt data with KMS

--- aws/aws.py
+++ aws/aws.py
@@ -1,4 +1,25 @@
 import boto3
+from dotenv import load_dotenv
+import os
+import base64
 
+load_dotenv()
 
-kms = boto3.resource('kms')
+# Data as read from a txt file
+# data = bytes(open('raw.txt', 'r').read().encode(encoding="ascii"))
+
+def encrypt(data):
+    kms = boto3.client('kms')
+    res = kms.encrypt(
+        KeyId=os.getenv('KEY_ID'),
+        Plaintext=data,
+        EncryptionAlgorithm="SYMMETRIC_DEFAULT"
+    )
+
+    return(base64.b64encode(res['CiphertextBlob']))
+
+def write_out(res):
+    with open('encrypted.txt', 'w') as f:
+        f.write(res.decode('ascii'))
+        f.close()
+    print('Written to file.')
