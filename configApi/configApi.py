10de9ed03b486bdfab697a2f933bf7d79095cdb6
DralrinResthal <c.neilclack@gmail.com>
2021-03-05 08:45:34 -0600

    Add basic JWT check

--- configApi/configApi.py
+++ configApi/configApi.py
@@ -22,59 +22,74 @@ a_log = CustomLogger(
 
 @app.route("/getParams", methods=["POST"])
 def getParams():
-    git.clone(
-        uri="github.com/DralrinResthal/ConfigTestRepo",
-        target="git_repo",
-        token=os.getenv("ACCESS_TOKEN"),
-    )
-    data = file.read_yaml("git_repo/example.yml")
-    a_log.info("Repository cloned and data decrypted.")
+    authToken = request.json["authToken"]
+    if authToken == os.getenv("AUTH_TOKEN"):
+        git.clone(
+            uri="github.com/DralrinResthal/ConfigTestRepo",
+            target="git_repo",
+            token=os.getenv("ACCESS_TOKEN"),
+        )
+        data = file.read_yaml("git_repo/example.yml")
+        a_log.info("Repository cloned and data decrypted.")
 
-    return jsonify(json.loads(data)), 200
+        return jsonify(json.loads(data)), 200
+    else:
+        return "Invalid Auth Token", 403
 
 
 @app.route("/putParams", methods=["POST"])
 def putParams():
-    now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    data = request.json
-    params = data["parameters"]
-    user = data["userInfo"]
-    title = f"Config Change - {now}"
-    msg = f"Created by: {user['userName']} at {now}"
-    params = file.check_secret(json.dumps(params))
-    file.write_file(params, "git_repo/example.yml")
-    git.pull("git_repo")
-    branch = git.new_branch("git_repo")
-    git.add_commit(
-        "git_repo",
-        ["example.yml"],
-        "Update to example.yml parameters",
-        user["userName"],
-        user["userEmail"],
-    )
-    git.create_pr(
-        "DralrinResthal/ConfigTestRepo",
-        "git_repo",
-        os.getenv("ACCESS_TOKEN"),
-        title,
-        msg,
-        branch,
-        "main",
-    )
-    return jsonify({"fileUpdate": "Success", "Add and Commit": "Success"}), 200
+    authToken = request.json["authToken"]
+    if authToken == os.getenv("AUTH_TOKEN"):
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        data = request.json
+        params = data["parameters"]
+        user = data["userInfo"]
+        title = f"Config Change - {now}"
+        msg = f"Created by: {user['userName']} at {now}"
+        params = file.check_secret(json.dumps(params))
+        file.write_file(params, "git_repo/example.yml")
+        git.pull("git_repo")
+        branch = git.new_branch("git_repo")
+        git.add_commit(
+            "git_repo",
+            ["example.yml"],
+            "Update to example.yml parameters",
+            user["userName"],
+            user["userEmail"],
+        )
+        git.create_pr(
+            "DralrinResthal/ConfigTestRepo",
+            "git_repo",
+            os.getenv("ACCESS_TOKEN"),
+            title,
+            msg,
+            branch,
+            "main",
+        )
+        return (
+            jsonify({"fileUpdate": "Success", "Add and Commit": "Success"}),
+            200,
+        )
+    else:
+        return "Invalid Auth Token", 403
 
 
 @app.route("/storeParams", methods=["POST"])
 def storeParams():
-    data = file.read_yaml(t_filepath)
-    res = aws.store(data)
-    msg = {}
-    for i in res:
-        n = 1
-        msg[n] = i
-        n += 1
+    authToken = request.json["authToken"]
+    if authToken == os.getenv("AUTH_TOKEN"):
+        data = file.read_yaml(t_filepath)
+        res = aws.store(data)
+        msg = {}
+        for i in res:
+            n = 1
+            msg[n] = i
+            n += 1
 
-    return jsonify(msg), 200
+        return jsonify(msg), 200
+    else:
+        return "Invalid Auth Token", 403
 
 
 if __name__ == "__main__":
