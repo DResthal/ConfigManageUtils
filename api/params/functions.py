d742ab0f104c96cb00add0721d700e344f78d28d
DralrinResthal <c.neilclack@gmail.com>
2021-09-15 21:39:12 -0500

    Add decryption method

--- api/params/functions.py
+++ api/params/functions.py
@@ -4,6 +4,8 @@ from flask import current_app
 
 
 def enc(data: str) -> str:
+
+    # KMS requires a bytes object, convert to ascii then bytes here
     data = bytes(data.encode("ascii"))
     bsess = boto3.Session(profile_name="default")
     kms = bsess.client("kms")
@@ -13,4 +15,21 @@ def enc(data: str) -> str:
         EncryptionAlgorithm="SYMMETRIC_DEFAULT",
     )
 
+    # 09/15/2021
+    # KMS returns an object with an attribute named CiphertextBlob, this needs to be encoded
+    # Decode the response to ascii, then encode that response in Base64
+    # I honestly don't remember why, but this is the only way it works.
     return base64.b64encode(res["CiphertextBlob"]).decode("ascii")
+
+
+def dec(data: str) -> str:
+    # data starts off as a base64 encoded string, decode this to feed into KMS
+    # Again, I don't remember why b64 is necessary, but this is the only way it works.
+    data = base64.b64decode(data)
+    bsess = boto3.Session(profile_name="default")
+    kms = bsess.client("kms")
+    r = kms.decrypt(CiphertextBlob=data)
+
+    # KMS decrypt returns a bytes string as Plaintext attribute
+    # Decode this bytes string to ascii for python.
+    return r["Plaintext"].decode("ascii")
