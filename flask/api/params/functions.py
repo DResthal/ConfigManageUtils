fb8062adf6080ce337d6114fc7d915bc7e56691e
DralrinResthal <c.neilclack@gmail.com>
2021-10-04 10:45:32 -0400

    Added docstrings to functions

--- flask/api/params/functions.py
+++ flask/api/params/functions.py
@@ -4,6 +4,17 @@ from flask import current_app
 
 
 def redacted(data: list) -> list:
+    """
+    Replace the key "value" of a dictionary object with "REDACTED", if the dictionary contains a "secret":"True" key:value pair
+
+    Parameters
+    ----------
+    data: List of dictionaries to check and update
+
+    Response
+    --------
+    List of dictionaries as provided with the replaced value "REDACTED"
+    """
     for i in data:
         if i["secret"]:
             i["value"] = "REDACTED"
@@ -24,6 +35,17 @@ def prefix_names(data: list) -> list:
 
 
 def enc(data: str) -> str:
+    """
+    Encrypts the given string data with AWS KMS
+
+    Parameters
+    ----------
+    data: String to encrypt
+
+    Response
+    --------
+    String
+    """
     kms = boto3.client("kms")
     res = kms.encrypt(
         KeyId=current_app.config["KMS_KEY_ID"],
@@ -36,6 +58,17 @@ def enc(data: str) -> str:
 
 
 def dec(data: str) -> str:
+    """
+    Decrypts an encrypted string with AWS KMS
+
+    Parameters
+    ----------
+    data: String to decrypt: Ensure this is an encrypted string and not a raw string or encoding will break.
+
+    Response
+    --------
+    String
+    """
     data = base64.b64decode(data)
     kms = boto3.client("kms")
     res = kms.decrypt(CiphertextBlob=data)
@@ -44,6 +77,17 @@ def dec(data: str) -> str:
 
 
 def store_ps(data: list) -> list:
+    """
+    Stores data in AWS Parameter store under SSM
+
+    Parameters
+    ----------
+    data: List of dictionaries representing parameters to store
+
+    Response
+    --------
+    List of AWS SSM responses as dictionaries for each parameter stores.
+    """
     ssm = boto3.client("ssm")
 
     response_list = []
