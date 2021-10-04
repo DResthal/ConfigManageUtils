e9c5df3032021b11924ec72fbc283f7a25f4f603
DralrinResthal <c.neilclack@gmail.com>
2021-10-04 10:46:21 -0400

    Remove unused decorators

--- flask/api/extensions.py
+++ flask/api/extensions.py
@@ -12,32 +12,3 @@ a_log = getLogger("alog")
 db = SQLAlchemy()
 ma = Marshmallow()
 basic_auth = BasicAuth()
-
-
-def authorized(func):
-    @wraps(func)
-    def auth_wrapper(*args, **kwargs):
-        data = request.json
-
-        if not data["authToken"]:
-            return "Unauthorized", 402
-
-        if data["authToken"] != current_app.config["AUTHTOKEN"]:
-            return "Unauthorized", 402
-
-        return func(*args, **kwargs)
-
-    return auth_wrapper
-
-
-def env_req(func):
-    @wraps(func)
-    def env_wrapper(*args, **kwargs):
-        data = request.json
-
-        if not data["env"]:
-            return "env Required", 402
-
-        return func(*args, **kwargs)
-
-    return env_wrapper
